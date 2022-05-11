8dc1980736532e3fccc53e50fdf982a93439bcd0
ushioda <yusuke_ushioda@mediado.jp>
2022-05-11 16:01:17 +0900

    add login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    
+    
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new #登録フォームの入力値
+        if @user.save
+          # ユーザ登録に成功した場合の処理
+          redirect_to user_path(@user.id)
+        else
+          # ユーザ登録に失敗した場合の処理
+          render :new
+        end
+    end
+
+    
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :meetings
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'meetings#index'
+  resources :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
 end
