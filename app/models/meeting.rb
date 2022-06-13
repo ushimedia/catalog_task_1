bd9d8ca83b3585fe442b1f85678b88863cef359c
ushioda <yusuke_ushioda@mediado.jp>
2022-06-13 16:09:33 +0900

    add

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,11 @@
 class Meeting < ApplicationRecord
     belongs_to :user
     validates :name, :goal, presence: true
+    validate :start_end_check, on: :update
     enum status: { 完了: 0, 議論中: 1, 保留: 2 }
-
+    
+    def start_end_check
+        errors.add(:end_meeting, "の時間は会議開始より後の時刻で修正ください。") unless
+        self.start_meeting < self.end_meeting
+      end
 end
