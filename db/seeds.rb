71693a71bb06c254fef49fd1bb3d796187baed94
ushioda <yusuke_ushioda@mediado.jp>
2022-06-13 19:00:14 +0900

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,27 @@ User.create(name: '西谷誉', email: 'test6@yahoo.co.jp', password: 'test666', 
 5.times do |n|
     Meeting.create!(
       name: "会議#{n + 1}",
-      agenda: "本日のアジェンダその#{n + 1}",
-      goal: "結論を出す",
+      agenda: "本日のアジェンダ#{n + 1}",
+      goal: "各支社の進捗確認",
       status: 1,
       user_id: 1,
       hold_time: 30,
-      start_meeting: '2022-05-18 12:33:44', 
-      end_meeting: '2022-05-18 12:44:55'
+      minutes: "スーパーセールのADマイルがYoY+20%"
+      start_meeting: '2022-05-18 9:40:00', 
+      end_meeting: '2022-05-18 10:00:00'
     )
   end
 
   5.times do |n|
     Meeting.create!(
       name: "会議#{n + 1}",
-      agenda: "本日のアジェンダその#{n + 1}",
-      goal: "結論を出す",
+      agenda: "本日のアジェンダ#{n + 1}",
+      goal: "各支社の進捗確認",
       status: 1,
       user_id: 2,
       hold_time: 30,
-      start_meeting: '2022-05-18 12:33:44', 
-      end_meeting: '2022-05-18 12:44:55'
+      start_meeting: '2022-05-18 9:40:00', 
+      end_meeting: '2022-05-18 10:00:00'
     )
   end
 
